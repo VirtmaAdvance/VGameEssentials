ae1a72e99ddb86a91d010f0535b84ce2378b4041
Daniel K. Valente <warmasterprime.gamming@gmail.com>
2025-03-28 12:40:43 -1000

    Created integer extension class.

---
+++ Systems/Extensions/Numbers/Integers/IntIsBetweenExt.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VGameEssentials.Systems.Extensions.Numbers.Integers
+{
+	public static class IntIsBetweenExt
+	{
+
+
+
+	}
+}
