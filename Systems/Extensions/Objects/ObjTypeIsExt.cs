54e6bfa8ca0b7c9b1d20df7319c205fbf4066d9a
Daniel K. Valente <warmasterprime.gamming@gmail.com>
2025-03-28 13:30:34 -1000

    Created object type inheritence extension class.

---
+++ Systems/Extensions/Objects/ObjTypeIsExt.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VGameEssentials.Systems.Extensions.Objects
+{
+	/// <summary>
+	/// Provides data-type validation extension methods for objects.
+	/// </summary>
+	public static class ObjTypeIsExt
+	{
+
+
+
+	}
+}
