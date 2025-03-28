c7b52cb9ae97c57a06b4b047f1762476caa6032f
Daniel K. Valente <warmasterprime.gamming@gmail.com>
2025-03-28 12:44:50 -1000

    Created IsBetween extension method for integers.

--- Systems/Extensions/Numbers/Integers/IntIsBetweenExt.cs
+++ Systems/Extensions/Numbers/Integers/IntIsBetweenExt.cs
@@ -8,8 +8,22 @@ namespace VGameEssentials.Systems.Extensions.Numbers.Integers
 {
 	public static class IntIsBetweenExt
 	{
-
-
+		/// <summary>
+		/// Determines if the <paramref name="source"/> value is equal to or between both the <paramref name="min"/> and <paramref name="max"/> values.
+		/// </summary>
+		/// <param name="source">The value to analyze.</param>
+		/// <param name="min">The minimum value allowed for the <paramref name="source"/> to be.</param>
+		/// <param name="max">The maximum value allowed for the <paramref name="source"/> to be.</param>
+		/// <returns>a <see cref="bool"/> representation where <see cref="bool">true</see> results in success, otherwise <see cref="bool">false</see>.</returns>
+		/// <exception cref="ArgumentOutOfRangeException"></exception>
+		public static bool IsBetween(this int source, int min, int max)
+		{
+			if (min > max)
+				throw new ArgumentOutOfRangeException("The given minimum value is larger than the given maximum value.");
+			if (min == max)
+				throw new ArgumentOutOfRangeException("The given minimum value cannot be equal to the given maximum value.");
+			return source >= min && source <= max;
+		}
 
 	}
 }
