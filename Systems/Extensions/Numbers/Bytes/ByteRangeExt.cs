4de16a68cfd087dd2f14252c4a39f6f833adb78b
Daniel K. Valente <warmasterprime.gamming@gmail.com>
2025-03-28 12:47:53 -1000

    Updated byte range validation extension.

--- Systems/Extensions/Numbers/Bytes/ByteRangeExt.cs
+++ Systems/Extensions/Numbers/Bytes/ByteRangeExt.cs
@@ -1,12 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace VGameEssentials.Systems.Extensions.Numbers.Bytes
+﻿namespace VGameEssentials.Systems.Extensions.Numbers.Bytes
 {
-	internal class ByteRangeExt
+	/// <summary>
+	/// Provides range validation extension methods.
+	/// </summary>
+	public static class ByteRangeExt
 	{
+		/// <inheritdoc cref="Integers.IntRangeExt.IsBetween(int, int, int)"/>
+		public static bool IsBetween(this byte source, byte min, byte max)
+		{
+			if (min > max)
+				throw new ArgumentOutOfRangeException("The given minimum value is larger than the given maximum value.");
+			if (min == max)
+				throw new ArgumentOutOfRangeException("The given minimum value cannot be equal to the given maximum value.");
+			return source >= min && source <= max;
+		}
+
 	}
 }
