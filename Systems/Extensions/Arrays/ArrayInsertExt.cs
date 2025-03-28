6065782b29c90a7ebf7838bd362432be01beac01
Daniel K. Valente <warmasterprime.gamming@gmail.com>
2025-03-28 12:37:26 -1000

    Created array insertion extension class.

---
+++ Systems/Extensions/Arrays/ArrayInsertExt.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VGameEssentials.Systems.Extensions.Arrays
+{
+	public static class ArrayInsertExt
+	{
+
+		public static T?[] Insert<T>(this T?[]? source, T? item, int indexPositionToSetItemAt)
+		{
+			if (source is null)
+				source = [];
+
+		}
+
+
+	}
+}
