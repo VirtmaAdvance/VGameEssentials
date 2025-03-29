26691983cebdf18ce4e5848cdba835ccc9e1316c
Daniel K. Valente <warmasterprime.gamming@gmail.com>
2025-03-28 14:25:18 -1000

    Completed the Move extension method for array objects.

--- Systems/Extensions/Arrays/ArrayMoveExt.cs
+++ Systems/Extensions/Arrays/ArrayMoveExt.cs
@@ -1,25 +1,38 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using VGameEssentials.Systems.Extensions.Numbers.Ints;
+﻿using VGameEssentials.Systems.Extensions.Numbers.Ints;
 
 namespace VGameEssentials.Systems.Extensions.Arrays
 {
+	/// <summary>
+	/// Provides array item movement extension methods.
+	/// </summary>
 	public static class ArrayMoveExt
 	{
-
-		public static void Move<T>(this T?[]? source, int fromIndex, int toIndex)
+		/// <summary>
+		/// Moves the value at the <paramref name="fromIndex"/> position to the <paramref name="toIndex"/> position within the array.
+		/// </summary>
+		/// <typeparam name="T">The data-type of the items within the array.</typeparam>
+		/// <param name="source">The source array to process.</param>
+		/// <param name="fromIndex">The index position of the value that will be moved to the <paramref name="toIndex"/> position.</param>
+		/// <param name="toIndex">The index position of the value that will be moved to the <paramref name="fromIndex"/> position.</param>
+		/// <returns>the modified array.</returns>
+		public static T?[] Move<T>(this T?[]? source, int fromIndex, int toIndex)
 		{
-			if (source is null)
-				source = [];
-			if(!fromIndex.IsBetween(0, source.Length-1))
-			{
-
-			}
-
-			var valueAtFromIndex = source[fromIndex];
+			source ??= [];
+			T? valueAtFromIndex, valueAtToIndex;
+			var sourceMaxIndexableValue = source.Length - 1;
+			int lowestIndexValue = Math.Min(fromIndex, toIndex);
+			int highestIndexValue = Math.Max(fromIndex, toIndex);
+			lowestIndexValue = Math.Min(lowestIndexValue, highestIndexValue).GetClosestValue(0, sourceMaxIndexableValue); // The lowest possible index.
+			highestIndexValue = Math.Max(lowestIndexValue, highestIndexValue); // The highest index specified by the caller.
+			if (!fromIndex.IsBetween(0, sourceMaxIndexableValue))
+				fromIndex = fromIndex.GetClosestValue(0, sourceMaxIndexableValue);
+			if (!toIndex.IsBetween(0, sourceMaxIndexableValue))
+				Array.Resize(ref source, source.Length + (Math.Max(highestIndexValue, sourceMaxIndexableValue) - Math.Min(highestIndexValue, sourceMaxIndexableValue)));
+			valueAtFromIndex = source[fromIndex];
+			valueAtToIndex = source[toIndex];
+			source[toIndex] = valueAtFromIndex;
+			source[fromIndex] = valueAtToIndex;
+			return source;
 		}
 
 	}
