e92aa970a471adb5aca14d7b016f7b698de378bc
Daniel K. Valente <warmasterprime.gamming@gmail.com>
2025-03-28 15:33:13 -1000

    Updated the GameObject class.

--- Systems/GameObject.cs
+++ Systems/GameObject.cs
@@ -8,11 +8,11 @@ namespace VGameEssentials.Systems
 	public class GameObject
 	{
 
-		private static VEnumerable<GameObject> s_items = [];
+		private static VEnumerable<GameObject> s_gameObjects = [];
 		/// <summary>
 		/// The game objects that have been created.
 		/// </summary>
-		public static VEnumerable<GameObject> Items => s_items;
+		public static VEnumerable<GameObject> GameObjects => s_gameObjects;
 		/// <summary>
 		/// The ID for this game object.
 		/// </summary>
@@ -23,13 +23,29 @@ namespace VGameEssentials.Systems
 		public string? Name { get; set; }
 
 
-		/// <summary>
-		/// Creates a new instance of the <see cref="GameObject"/> class.
-		/// </summary>
-		/// <param name="name"></param>
+		/// <inheritdoc cref="GameObject.GameObject(Guid, string)"/>
+		public GameObject()
+		{
+			ID = Guid.NewGuid();
+			GameObjects.Add(this);
+		}
+		/// <inheritdoc cref="GameObject.GameObject(Guid, string)"/>
 		public GameObject(string name)
 		{
 			ID = Guid.NewGuid();
+			Name = name;
+			GameObjects.Add(this);
+		}
+		/// <summary>
+		/// Creates a new instance of the <see cref="GameObject"/> class.
+		/// </summary>
+		/// <param name="id">The ID for this object.</param>
+		/// <param name="name">The name for this object.</param>
+		public GameObject(Guid id, string name)
+		{
+			ID = id;
+			Name = name;
+			GameObjects.Add(this);
 		}
 
 	}
