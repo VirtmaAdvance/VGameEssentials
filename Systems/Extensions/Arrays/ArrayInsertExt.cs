4da5986e92c1f2b0adb12df6255cbb79ca236a19
Daniel K. Valente <warmasterprime.gamming@gmail.com>
2025-03-28 14:35:51 -1000

    Completed the Insert extension method for array objects.

--- Systems/Extensions/Arrays/ArrayInsertExt.cs
+++ Systems/Extensions/Arrays/ArrayInsertExt.cs
@@ -1,21 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace VGameEssentials.Systems.Extensions.Arrays
+﻿namespace VGameEssentials.Systems.Extensions.Arrays
 {
+	/// <summary>
+	/// Provides insertion extension methods for arrays.
+	/// </summary>
 	public static class ArrayInsertExt
 	{
-
+		/// <summary>
+		/// Inserts the <paramref name="item"/> into the <paramref name="source"/> array (Invokes Shift and Move extension methods).
+		/// </summary>
+		/// <typeparam name="T">The data-type of the items within the source array.</typeparam>
+		/// <param name="source">The array to process.</param>
+		/// <param name="item">The item to insert into the array.</param>
+		/// <param name="indexPositionToSetItemAt">The index position to insert the <paramref name="item"/> at.</param>
+		/// <returns>the modified array.</returns>
 		public static T?[] Insert<T>(this T?[]? source, T? item, int indexPositionToSetItemAt)
 		{
 			if (source is null)
 				source = [];
-
+			source = source.Shift(indexPositionToSetItemAt, 1);
+			source[indexPositionToSetItemAt] = item;
+			return source;
 		}
-
 
 	}
 }
