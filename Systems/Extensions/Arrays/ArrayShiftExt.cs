8b6c47df7db3ed54689b3aed017979b8c7f50999
Daniel K. Valente <warmasterprime.gamming@gmail.com>
2025-03-28 14:26:21 -1000

    Created array shift extension class.

---
+++ Systems/Extensions/Arrays/ArrayShiftExt.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VGameEssentials.Systems.Extensions.Arrays
+{
+	/// <summary>
+	/// Provides item shifting extensions for arrays.
+	/// </summary>
+	public static class ArrayShiftExt
+	{
+
+
+
+	}
+}
